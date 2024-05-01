204d178ad47b39c239f78c1bc8b95083864abea5
KarinaZakharian <karina.zakharian@oclock.school>
2024-05-01 15:18:25 +0200

    img update

--- src/store/reducers/card.ts
+++ src/store/reducers/card.ts
@@ -15,7 +15,12 @@ interface Apartment {
   latitude: number;
   longitude: number;
 }
-
+import image1 from '../../assets/1.jpg';
+import image2 from '../../assets/2.jpg';
+import image3 from '../../assets/3.jpg';
+import image4 from '../../assets/4.jpg';
+import image5 from '../../assets/5.jpg';
+import image6 from '../../assets/6.jpg';
 
 
 const initialState=[
@@ -24,7 +29,7 @@ const initialState=[
     "name": "Cozy Studio Apartment in Lviv",
     "description": "This cozy studio apartment is located in the heart of Lviv's historic center. It features modern amenities and a comfortable atmosphere, perfect for solo travelers or couples.",
     "price": '50',
-    "photo": "/assets/1.jpg",
+    "photo": image1,
     "user_address": "Lviv, Ukraine",
     "latitude": 49.8397,
     "longitude": 24.0297
@@ -34,7 +39,7 @@ const initialState=[
     "name": "Spacious Loft in Kyiv City Center",
     "description": "Enjoy the vibrant atmosphere of Kyiv with this spacious loft apartment. Located in the city center, it offers easy access to attractions, restaurants, and nightlife.",
     "price": '80',
-    "photo": "/assets/2.jpg",
+    "photo": image2,
     "user_address": "Kyiv, Ukraine",
     "latitude": 50.4501,
     "longitude": 30.5234
@@ -44,7 +49,7 @@ const initialState=[
     "name": "Modern Apartment in Odesa",
     "description": "Experience the beauty of Odesa with this modern apartment. Situated near the Black Sea coast, it offers stunning views and a relaxing atmosphere.",
     "price": '65',
-    "photo": "/assets/3.jpg",
+    "photo": image3,
     "user_address": "Odesa, Ukraine",
     "latitude": 46.4694,
     "longitude": 30.7409
@@ -54,7 +59,7 @@ const initialState=[
     "name": "Charming Flat in Kharkiv",
     "description": "Discover the charm of Kharkiv with this lovely flat. Located in a quiet neighborhood, it's perfect for those seeking a peaceful retreat.",
     "price": '55',
-    "photo": "/assets/4.jpg",
+    "photo": image4,
     "user_address": "Kharkiv, Ukraine",
     "latitude": 49.9935,
     "longitude": 36.2304
@@ -64,7 +69,7 @@ const initialState=[
     "name": "Riverside Apartment in Dnipro",
     "description": "Enjoy the tranquility of the Dnipro River with this riverside apartment. Ideal for nature lovers, it offers beautiful views and outdoor activities.",
     "price": '70',
-    "photo": "/assets/5.jpg",
+    "photo": image5,
     "user_address": "Dnipro, Ukraine",
     "latitude": 48.4647,
     "longitude": 35.0462
@@ -74,7 +79,7 @@ const initialState=[
     "name": "Historic House in Chernivtsi",
     "description": "Step back in time with this historic house in Chernivtsi. Featuring traditional architecture and antique furnishings, it offers a unique and memorable stay.",
     "price": '90',
-    "photo": "/assets/6.jpg",
+    "photo": image6,
     "user_address": "Chernivtsi, Ukraine",
     "latitude": 48.2921,
     "longitude": 25.9359
@@ -84,7 +89,7 @@ const initialState=[
     "name": "Mountain Retreat in Carpathians",
     "description": "Escape to the Carpathian Mountains with this mountain retreat. Surrounded by breathtaking scenery, it's the perfect getaway for outdoor enthusiasts.",
     "price": '100',
-    "photo": "/assets/3.jpg",
+    "photo": image1,
     "user_address": "Yaremche, Ukraine",
     "latitude": 48.4494,
     "longitude": 24.5531
@@ -94,7 +99,7 @@ const initialState=[
     "name": "Seaside Apartment in Odessa",
     "description": "Experience the beauty of the Black Sea coast with this seaside apartment in Odessa. Enjoy stunning sunsets and easy access to the beach.",
     "price": '75',
-    "photo": "/assets/2.jpg",
+    "photo": image2,
     "user_address": "Odessa, Ukraine",
     "latitude": 46.4694,
     "longitude": 30.7409
@@ -104,7 +109,7 @@ const initialState=[
     "name": "Luxury Penthouse in Kyiv",
     "description": "Indulge in luxury with this penthouse apartment in Kyiv. Featuring upscale amenities and panoramic views of the city skyline, it offers an unforgettable experience.",
     "price":' 150',
-    "photo": "/assets/1.jpg",
+    "photo": image3,
     "user_address": "Kyiv, Ukraine",
     "latitude": 50.4501,
     "longitude": 30.5234
